65ebf8fc1ef941beecb7e715e321f24af2921165
Justin Stitt <jstitt007@gmail.com>
2024-12-06 02:04:32 -0800

    upd temp

--- template/next.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-#
-
-cp solve.py solve2.py
-touch small2.in
-lvim solve2.py
--- template/solve.py
+++ template/solve.py
@@ -3,8 +3,9 @@ import itertools
 import functools
 import re
 import time
+import pyperclip
 from icecream import ic
-from collections import defaultdict
+from collections import defaultdict, Counter
 from pathlib import Path
 
 
@@ -13,16 +14,18 @@ class Soln:
         with open(input_file, "r") as fd:
             self._raw = fd.read()
             self.lines = [line for line in self._raw.strip().split("\n")]
-            self.mat = {
-                (r, c): char
-                for r, row in enumerate(self.lines)
-                for c, char in enumerate(row)
-            }
+            self.mat = defaultdict(
+                lambda: None,
+                {
+                    (r, c): char
+                    for r, row in enumerate(self.lines)
+                    for c, char in enumerate(row)
+                },
+            )
             self.n = len(self.lines)
             self.m = len(self.lines[0])
 
-            # ensure square (n x m)
-            assert all([len(self.lines[i]) == self.n for i in range(self.n)])
+            ic(self.n, self.m)
 
     def solve(self):
         for line in self.lines:
@@ -30,14 +33,15 @@ class Soln:
 
 
 # fmt: off
-# python solve.py <input> {-d <disable icecream output>}
+# Usage: $ python solve.py <input> {-d <disable icecream output>}
 if __name__ == "__main__":
     if len(sys.argv) > 2 and sys.argv[2] == "-d": ic.disable()
     else: ic.configureOutput(includeContext=True)
     cwd = Path(__file__).parent.resolve()
-    input_file = cwd / Path(sys.argv[1]).with_suffix('in') if len(sys.argv) > 1 else cwd / Path("small.in")
+    input_file = cwd / Path(sys.argv[1]).with_suffix('.in') if len(sys.argv) > 1 else cwd / Path("small.in")
     time_start = time.perf_counter()
     soln = Soln(input_file=input_file)
     ans = soln.solve()
-    print(f"ans: {ans}")
+    print(f"ans: <<{ans}>>")
     print(f'Solved in {time.perf_counter()-time_start:.5f} Sec.')
+    pyperclip.copy(str(ans))
