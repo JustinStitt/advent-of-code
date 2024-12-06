dfb0f26de4f81dfe4b48dbec75aa7ebb1dadfa7c
Justin Stitt <jstitt007@gmail.com>
2024-12-06 01:52:30 -0800

    upds

--- template/solve.py
+++ template/solve.py
@@ -1,19 +1,28 @@
-#! /home/jstitt/repos/advent-of-code-2023/.venv/bin/python3
-# Advent of Code 2023 - Day TODO
 import sys
 import itertools
 import functools
 import re
+import time
 from icecream import ic
 from collections import defaultdict
+from pathlib import Path
 
 
 class Soln:
-    def __init__(self, inp_file):
-        with open(inp_file, "r") as fd:
-            self.lines = [x.strip() for x in fd.readlines()]
-            self.rows = len(self.lines)
-            self.cols = len(self.lines[0])
+    def __init__(self, input_file: Path):
+        with open(input_file, "r") as fd:
+            self._raw = fd.read()
+            self.lines = [line for line in self._raw.strip().split("\n")]
+            self.mat = {
+                (r, c): char
+                for r, row in enumerate(self.lines)
+                for c, char in enumerate(row)
+            }
+            self.n = len(self.lines)
+            self.m = len(self.lines[0])
+
+            # ensure square (n x m)
+            assert all([len(self.lines[i]) == self.n for i in range(self.n)])
 
     def solve(self):
         for line in self.lines:
@@ -21,8 +30,14 @@ class Soln:
 
 
 # fmt: off
+# python solve.py <input> {-d <disable icecream output>}
 if __name__ == "__main__":
     if len(sys.argv) > 2 and sys.argv[2] == "-d": ic.disable()
     else: ic.configureOutput(includeContext=True)
-    soln = Soln(sys.argv[1] + ".in" if len(sys.argv) > 1 else "small.in")
-    soln.solve()
+    cwd = Path(__file__).parent.resolve()
+    input_file = cwd / Path(sys.argv[1]).with_suffix('in') if len(sys.argv) > 1 else cwd / Path("small.in")
+    time_start = time.perf_counter()
+    soln = Soln(input_file=input_file)
+    ans = soln.solve()
+    print(f"ans: {ans}")
+    print(f'Solved in {time.perf_counter()-time_start:.5f} Sec.')
